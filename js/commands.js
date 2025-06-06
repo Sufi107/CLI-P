1c5dffd43155a77c8f56527c0b4c02990c58a81f
Mohammed sufiyan <mohsufiyan.107@gmail.com>
2025-06-06 14:16:34 +0530

    Add files via upload

---
+++ js/commands.js
@@ -0,0 +1,169 @@
+achievements = [
+    "<br>",
+    "Azure Fundamentals",
+    "   - Issuing Organization: Microsoft",
+    "   - Issue Date: May 30, 2024",
+    "<br>",
+    "Azure AI Fundamentals",
+    "   - Issuing Organization: Microsoft",
+    "   - Issue Date: December, 2024",
+    "<br>",
+    "Data Science Methodology",
+    "   - Issuing Organization: IBM",
+    "   - Issue Date: March, 2025",
+    "   - Credential ID: DS0103EN",
+    "<br>"
+];
+
+clear = [
+    "<br>",
+    "The terminal has been cleared and is ready for your next command!",
+    "<br>"
+];
+
+contact = [
+    "<br>",
+    "Contact Information:",
+    "- Email: <a href='mailto:mohsufiyan.107@gmail.com' target='_blank' class='underlined-link'>mohsufiyan.107@gmail.com</a>",
+    "- LinkedIn: <a href='https://www.linkedin.com/in/MohammedSufiyan107/' target='_blank' class='underlined-link'>SufiyanMohammed</a>",
+    "- X: <a href='https://Instagram.com/msufiyan_107' target='_blank' class='underlined-link'>@msufiyan_107</a>",
+    "<br>"
+];
+
+education = [
+    "<br>",
+    "Education:",
+    "- Degree: BSc. (Honors) Cloud Computing and Big Data",
+    "- University: Reva University",
+    "- Graduation Year: 2025",
+    "<br>"
+];
+
+experience = [
+    "<br>",
+    "Professional Experience:",
+    "- Job Title: Cloud Intern",
+    "- Company: Plasmid Innovation Private Limited",
+    "- Duration: May 2024 - Present",
+    "   - Collaborating with Cloud teams.",
+    "   - Implementing automation tools to streamline cloud deployments.",
+    "<br>"
+];
+
+header = [
+    '<span class="index">Mohammed Sufiyan. All rights reserved ©️ 2025.</span>',
+    "<br>",
+    " ██████  ██████  ███    ███ ███    ███  █████  ███    ██ ██████      ██      ██ ███    ██ ███████ ",
+    "██      ██    ██ ████  ████ ████  ████ ██   ██ ████   ██ ██   ██     ██      ██ ████   ██ ██      ",
+    "██      ██    ██ ██ ████ ██ ██ ████ ██ ███████ ██ ██  ██ ██   ██     ██      ██ ██ ██  ██ █████   ",
+    "██      ██    ██ ██  ██  ██ ██  ██  ██ ██   ██ ██  ██ ██ ██   ██     ██      ██ ██  ██ ██ ██      ",
+    " ██████  ██████  ██      ██ ██      ██ ██   ██ ██   ████ ██████      ███████ ██ ██   ████ ███████ ",
+    "<br>",
+    "██████   █████  ███████ ███████ ██████      ██████   ██████  ██████  ████████ ███████  ██████  ██      ██  ██████  ",
+    "██   ██ ██   ██ ██      ██      ██   ██     ██   ██ ██    ██ ██   ██    ██    ██      ██    ██ ██      ██ ██    ██ ",
+    "██████  ███████ ███████ █████   ██   ██     ██████  ██    ██ ██████     ██    █████   ██    ██ ██      ██ ██    ██ ",
+    "██   ██ ██   ██      ██ ██      ██   ██     ██      ██    ██ ██   ██    ██    ██      ██    ██ ██      ██ ██    ██ ",
+    "██████  ██   ██ ███████ ███████ ██████      ██       ██████  ██   ██    ██    ██       ██████  ███████ ██  ██████  ",
+    "<br>",
+    '<span class="color2">Welcome to Command Line based Portfolio; Your Interactive Web Terminal Experience.</span>',
+    '<span class="color2">Explore the capabilities of this portfolio terminal. Type</span> <span class="command">\'help\'</span><span class="color2"> for a list of available commands.</span>',
+    "<br>"
+];
+
+help = [
+    "<br>",
+    "Available Commands:",
+    "- achievements: Highlight notable achievements, awards, or certifications.",
+    "- clear: Clears the terminal.",
+    "- contact: Display contact information for easy communication.",
+    "- education: Provide details about educational background.",
+    "- experience: Share information about professional work experience.",
+    "- help: Display a help menu with available commands.",
+    "- header: Display the header information with the Command Line based Portfolio banner.",
+    "- projects: List coding projects with descriptions and links.",
+    "- resume: Display or link to a downloadable resume.",
+    "- skills: Showcase technical skills, programming languages, and tools.",
+    "- social: Show links to social media profiles.",
+    "- whoami: Display personal information and a brief introduction.",
+    "<br>"
+];
+
+projects = [
+    "<br>",
+    "Projects:",
+    "1. Command Line based Portfolio",
+    "   - A sleek terminal-based portfolio styled with the one dark theme.",
+    "   - Link: <a href='https://github.com/Sufi107/CLI-P' target='_blank' class='underlined-link'>GitHub</a>",
+    "2. Todo List",
+    "   - A serverless todo list application built with React and Firebase.",
+    "   - Link: <a href='https://github.com/Sufi107/TODO-LIST' target='_blank' class='underlined-link'>GitHub</a>",
+    "3. Face Rekognition",
+    "   - A face recognition application using Python and OpenCV.",
+    "   - Link: <a href='https://github.com/Sufi107/face-rekognition-main' target='_blank' class='underlined-link'>GitHub</a>",
+    "<br>"
+];
+
+
+resume = [
+    "<br>",
+    "Download my resume:",
+    "- Link: <a href='https://drive.google.com/file/d/1MDQoVsa5yeZ5Ne5U5SAYhKc2FOdM869D/view?usp=drivesdk' target='_blank' class='underlined-link'>Download Resume</a>",
+    "<br>"
+];
+
+
+skills = [
+    "<br>",
+    "Technical Skills:",
+    "- Programming Languages: JavaScript, Python, HTML, CSS, etc.",
+    "- Web Technologies: HTML, CSS, React, Node.js, etc.",
+    "- Tools: Git, VSCode, Docker, etc.",
+    "<br>"
+];
+
+social = [
+    "<br>",
+    "Connect with me on social media:",
+    "- Discord: <a href='https://discord.com/users/1172427603513196565' target='_blank' class='underlined-link'>@scaar0_25044</a>",
+    "- GitHub: <a href='https://github.com/Sufi107/' target='_blank' class='underlined-link'>@Sufi107</a>",
+    "- Instagram: <a href='https://www.instagram.com/msufiyan_107/' target='_blank' class='underlined-link'>@msufiyan_107</a>",
+    "- LinkedIn: <a href='https://www.linkedin.com/in/MohammedSufiyan107/' target='_blank' class='underlined-link'>in/MohammedSufiyan107</a>",
+    "<br>"
+];
+
+
+whoami = [
+    "<br>",
+    "Welcome to CLI based Portfolio.",
+    "I'm Mohammed Sufiyan, a passionate Cloud Specialist.",
+    "This is my interactive terminal-based portfolio.",
+    "<br>"
+];
+
+// Secret Commands
+starwars = [
+    "<br>",
+    "⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⣤⣤⣤⣤⣤⣤⣤⣤⣤⣤⣤⣤⣤⣤⣤⣤⣤⣤⠀⢀⣤⣤⣤⣤⣤⣤⣤⠀⠀⠀⠀⢠⣤⣤⣤⣤⣤⣤⣤⣤⣤⣤⣀⠀⠀⠀⠀⠀⠀",
+    "⠀⠀⠀⠀⠀⠀⠀⠀⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⣼⣿⣿⣿⣿⣿⣿⣿⣇⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡀⠀⠀⠀⠀",
+    "⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⣿⣟⠛⠛⠛⠛⠛⣿⣿⣿⣿⡟⠛⠛⠛⠛⠛⢠⣿⣿⣿⣿⠿⣿⣿⣿⣿⡀⠀⠀⢸⣿⣿⣿⣿⡏⠉⠉⢉⣿⣿⣿⣿⡇⠀⠀⠀⠀",
+    "⠀⠀⠀⠀⠀⠀⠀⠀⠙⢿⣿⣿⣿⣿⣷⡀⠀⠀⠀⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⣾⣿⣿⣿⡟⠀⢻⣿⣿⣿⣧⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠀⠀⠀⠀⠀",
+    "⣤⣤⣤⣤⣤⣤⣤⣤⣤⣬⣿⣿⣿⣿⣿⣷⠀⠀⠀⣿⣿⣿⣿⡇⠀⠀⠀⠀⢰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡆⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⣤⣤⣤⣤⣤⣤",
+    "⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠏⠀⠀⠀⣿⣿⣿⣿⡇⠀⠀⠀⢀⣿⣿⣿⣿⡿⠿⠿⠿⠿⣿⣿⣿⣿⡀⢸⣿⣿⣿⣿⡇⠙⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿",
+    "⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠟⠛⠁⠀⠀⠀⠀⠿⠿⠿⠿⠇⠀⠀⠀⠸⠿⠿⠿⠟⠀⠀⠀⠀⠀⠻⠿⠿⠿⠇⠸⠿⠿⠿⠿⠇⠀⠀⠙⠛⠿⠿⠿⠿⠿⠿⠿⠿",
+    "⢻⣿⣿⣿⣿⡄⢠⣿⣿⣿⣿⣿⡆⠀⣾⣿⣿⣿⡟⠀⢀⣾⣿⣿⣿⣿⣿⣿⡄⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣷⣶⣦⣄⠀⠀⠀⠀⣠⣶⣶⣿⣿⣿⣿⣿⣿⣿⣿",
+    "⠈⣿⣿⣿⣿⣷⣼⣿⣿⣿⣿⣿⣿⣸⣿⣿⣿⣿⠁⠀⣼⣿⣿⣿⣿⣿⣿⣿⣷⠀⠀⠀⠀⣿⣿⣿⣿⣿⠿⠿⢿⣿⣿⣿⣿⣇⠀⠀⣸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿",
+    "⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡏⠀⢠⣿⣿⣿⣿⠏⢿⣿⣿⣿⣇⠀⠀⠀⣿⣿⣿⣿⣿⣀⣀⣀⣼⣿⣿⣿⡟⠀⠀⢹⣿⣿⣿⣿⣿⣄⠀⠀⠀⠀⠀",
+    "⠀⠀⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⣾⣿⣿⣿⣟⣀⣸⣿⣿⣿⣿⡀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠟⠁⠀⠀⠀⠙⢿⣿⣿⣿⣿⣧⠀⠀⠀⠀",
+    "⠀⠀⠘⣿⣿⣿⣿⣿⣿⠋⣿⣿⣿⣿⣿⣿⠇⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⠀⠀⣿⣿⣿⣿⣿⢿⣿⣿⣿⣿⣿⣶⣶⣶⣶⣶⣶⣾⣿⣿⣿⣿⣿⠇⠀⠀⠀",
+    "⠀⠀⠀⢻⣿⣿⣿⣿⡏⠀⢹⣿⣿⣿⣿⡟⠀⢀⣿⣿⣿⣿⡟⠛⠛⠛⠛⣿⣿⣿⣿⡆⠀⣿⣿⣿⣿⣿⠀⠙⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠀⠀⠀⠀",
+    "⠀⠀⠀⠈⠉⠉⠉⠉⠀⠀⠀⠉⠉⠉⠉⠁⠀⠈⠉⠉⠉⠉⠀⠀⠀⠀⠀⠉⠉⠉⠉⠉⠀⠉⠉⠉⠉⠉⠀⠀⠀⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠁⠀⠀⠀⠀⠀  ",
+    "<br>"
+];
+
+jokes = [
+    "<br>",
+    "1. What do you call a fake noodle? An Impasta.",
+    "2. What do you call a cow during an earthquake? A milkshake.",
+    "3. What do you call a cow with five legs? A cow.",
+    "<br>"
+];
